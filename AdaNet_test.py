b0b9738f9ca5a80e6ebeed372524449d5ceec41a
Marco Barbero Mota <marco.barbero.mota@vanderbilt.edu>
2023-10-06 10:54:29 -0500

    AdaNet_test (model test)

--- AdaNet_test.py
+++ AdaNet_test.py
@@ -1,26 +1,52 @@
 # %%
 import numpy as np
-from aenet.aenV2 import AdaptiveElasticNet
+from aenet import AdaptiveElasticNet
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score,average_precision_score, roc_curve
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
 from CML_tool.oracle_model_eval import compute_metrics
-from CML_tool.Utils import read_pickle
-import pandas as pd
 import time
 
 # %%
-# Import dataset
-file  = np.load(
-    '/home/barbem4/projects/Initial SLE dataset/Linear models SLE/Synthetic data/Data/oracle_dataset_1/n751_dt50_dn1950_corrpercent0_nstd0.05_corr0_TMcorrN/data.npz')
-for key in file:
-    exec(key + " = file['" + key + "']")
+#Generate data 
+X,y = make_classification(
+    n_samples = 751,
+    n_features = 2000,
+    n_informative= 50,
+    n_redundant= 40,
+    n_repeated = 0,
+    n_classes = 2,
+    n_clusters_per_class = 1,
+    random_state = 42
+)
 
+# Generate random weights
+weights = np.random.rand(X.shape[1])
+
+# Set some coefficients to zero
+fraction_of_zeros = 0.9
+# Calculate the number of elements to set to zero
+num_zeros = int(fraction_of_zeros * weights.size)
+
+# Generate a boolean mask with True values indicating which elements to set to zero
+mask = np.random.choice([True, False], size=weights.size, replace=True, p=[num_zeros/weights.size, 1 - num_zeros/weights.size])
+
+# Set the selected elements to zero
+weights[mask] = 0
+
+# Compute the linear response
+linear_response = np.dot(X, weights)
+
+# Apply the logistic link function to get y
+p = 1 / (1 + np.exp(-linear_response))
+# draw binary labels from binomial distribution
+y = np.random.binomial(1,p)
 # %%
 # define the models
-params_AdaNet = {'C': 1.5e-1,
- 'l1_ratio': 0.3160815048309521,
- 'nu': 1.232511206135,
+params_AdaNet ={'C':2.5,
+ 'l1_ratio': 0.9,
+ 'nu':0.0002,
  'gamma': 1}
 params_ENet = params_AdaNet.copy()
 del params_ENet['gamma']
@@ -30,7 +56,7 @@ del params_ENet['nu']
 ENet = LogisticRegression(
     penalty = 'elasticnet',
     warm_start=True, 
-    max_iter=4000,
+    max_iter=2000,
     solver = 'saga',
     tol = 1e-4
     )
@@ -39,15 +65,13 @@ ENet = LogisticRegression(
 AdaNet = AdaptiveElasticNet(
     AdaNet_solver_verbose=False,
     AdaNet_solver = 'default',
-    SIS_method = 'one-less',
     refinement=5,
     warm_start=True, 
-    max_iter=4000,
+    max_iter=2000,
     force_solver=False,
     printing_solver = False,
-    rescale_EN=False,
-    eps_constant=1e-8,
-    nonzero_tol='default',
+    rescale_EN=True,
+    eps_constant=1e-6,
     tol = 1e-4)
 
 # %%
@@ -55,13 +79,7 @@ AdaNet.set_params(**params_AdaNet)
 ENet.set_params(**params_ENet)
 
 # %%
-X_train, X_HOS, y_train, y_HOS = train_test_split(X_test, y_test, test_size=0.3, random_state=42)
-
-# X_train = X_train[:,0:60]
-# y_train = y_train[:]
-
-# X_HOS = X_HOS[:,0:60]
-# y_HOS = y_HOS[:]
+X_train, X_HOS, y_train, y_HOS = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # %%
 start_time_ENet = time.time()
@@ -77,13 +95,13 @@ print("AdaNet execution time:", end_time_AdaNet-start_time_AdaNet, "seconds")
 # %%
 # print the number of nonzero features
 print('ENet nonzero coefficients: ', sum(ENet.coef_.ravel()!=0))
-print('AdaNet-ENet nonzero coefficients: ', sum(AdaNet.enet_coef_.ravel()!=0))
+print('AdaNet-ENet nonzero coefficients: ', sum(AdaNet.ENet.coef_.ravel()!=0))
 print('AdaNet nonzero coefficients: ', sum(AdaNet.coef_.ravel()!=0))
 
 
 # %%
 # Test of coefficients
-print('ENet coefficient cummulative difference: ',sum(abs(ENet.coef_.ravel()-AdaNet.enet_coef_.ravel())))
+print('ENet coefficient cummulative difference: ',sum(abs(ENet.coef_.ravel()-AdaNet.ENet.coef_.ravel())))
 print('ENet-AdaNet coefficient cummulative difference: ',sum(abs(ENet.coef_.ravel()-AdaNet.coef_.ravel())))
 
 # %%
@@ -91,68 +109,28 @@ print('ENet-AdaNet coefficient cummulative difference: ',sum(abs(ENet.coef_.rave
 
 print('AUROC: ENet-sklearn({}), Ada-ENet({}) and AdaNet({})'.format(
 roc_auc_score(y_true=y_HOS, y_score=ENet.predict_proba(X_HOS)[:,1]),
-roc_auc_score(y_true=y_HOS, y_score=AdaNet.predict_proba_ENet(X_HOS)[:,1]),
+roc_auc_score(y_true=y_HOS, y_score=AdaNet.ENet.predict_proba(X_HOS)[:,1]),
 roc_auc_score(y_true=y_HOS, y_score=AdaNet.predict_proba(X_HOS)[:,1])
 ))
 print('AUCPR: ENet-sklearn({}), Ada-ENet({}) and AdaNet({})'.format(
     average_precision_score(y_true=y_HOS, y_score=ENet.predict_proba(X_HOS)[:,1]),
-    average_precision_score(y_true=y_HOS, y_score=AdaNet.predict_proba_ENet(X_HOS)[:,1]),
+    average_precision_score(y_true=y_HOS, y_score=AdaNet.ENet.predict_proba(X_HOS)[:,1]),
     average_precision_score(y_true=y_HOS, y_score=AdaNet.predict_proba(X_HOS)[:,1])
 ))
 
 # %%
 # Test oracle properties
 AdaNet_metrics_dict = compute_metrics(
-    true_model_coefs = file['beta_test'],
+    true_model_coefs = weights.ravel(),
     model_coefs = AdaNet.coef_.ravel()
     )
 ENet_metrics_dict = compute_metrics(
-    true_model_coefs = file['beta_test'],
+    true_model_coefs = weights.ravel(),
     model_coefs = ENet.coef_.ravel()
     )
 print("ENet: ", ENet_metrics_dict)
 print("AdaNet: ", AdaNet_metrics_dict)
-
 # %%
+
 print(AdaNet.AdaNet_solver)
-
-# %%
-data_SLE = read_pickle(path = '/home/barbem4/projects/Data/Initial Data', filename='marco_ids_final_dates_df_no_unknowns.pkl')
-labels = read_pickle(path = '/home/barbem4/projects/Data/Initial Data', filename='SLE_binary_labels_no_unknowns.pkl')
-features_names = np.load('/home/barbem4/projects/Data/Initial Data/raw_names.npy', allow_pickle=True)
-meta_df = read_pickle(path = '/home/barbem4/projects/Data/Initial Data', filename = 'meta.pkl')
-
-# %%
-AdaNet = AdaptiveElasticNet(
-    AdaNet_solver_verbose=False,
-    AdaNet_solver = 'default',
-    SIS_method = 'one-less',
-    refinement=5,
-    warm_start=True, 
-    max_iter=4000,
-    force_solver=False,
-    printing_solver = False,
-    rescale_EN=False,
-    eps_constant=1e-8,
-    nonzero_tol='default',
-    tol = 1e-4)
-
-params_AdaNet = {'C': 598.530099253463, 'l1_ratio': 0.4718544435940898, 'nu': 19.760031869591373, 'gamma': 1}
-
-AdaNet.set_params(**params_AdaNet)
-# %%
-AdaNet.fit(data_SLE, labels)
-
-# %%
-# print('ENet: ', np.array(features_names)[np.where(AdaNet.enet_coef_.ravel() != 0)[-1]])
-aucs = cross_val_score(
-    estimator = AdaNet,
-    X = data_SLE.values,
-    y = labels.values,
-    scoring = 'roc_auc',
-    cv = 10
-)
-print('10-fold CV: ', np.mean(aucs))
-print('AdaNet: ', np.array(features_names)[np.where(AdaNet.coef_.ravel() != 0)[-1]])
-
 # %%
